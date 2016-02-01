ae66344b7c1022931c40d8624ec1af4bff513b2c
Benjamin Riggs <deisum@gmail.com>
2016-01-31 19:11:54 -0600

    Mostly functional. Needs UI updates. Now testing workflow within XLMS framework.

--- AMM.js
+++ AMM.js
@@ -5,7 +5,8 @@
 
 
 let PHYSIOLOGY_VARIABLES = [
-    "BLOOD_PRESSURE_DIASYSTOLIC",
+    "BLOOD_LOSS",
+    "BLOOD_PRESSURE_DIASTOLIC",
     "BLOOD_PRESSURE_SYSTOLIC",
     "CORE_TEMP",
     "HEART_RATE",
@@ -18,6 +19,7 @@ let PHYSIOLOGY_VARIABLES = [
 var physiology_data = {};
 // Initialize physiology_data with PHYS_VARS keys, null values.
 PHYSIOLOGY_VARIABLES.forEach(value => physiology_data[value] = null);
+physiology_data.keys = Object.keys(physiology_data);
 
 
 let ui = {
@@ -28,15 +30,15 @@ let ui = {
 };
 
 
-var poller_callback = null;
-var poller_ID = null;
+var POLLER_CALLBACK = null;
+var POLLER_ID = null;
 
 
 var session = {
     scenario: "SCENARIO_1",
     log: [],
     attached_modules: [],
-    required_modules: [],
+    required_modules: ['XLMS'],
 };
 
 
@@ -50,29 +52,30 @@ var logger = function (message) {
 
 
 function get_AMM_messages(callback) {
-    fetch("http://" + session.IP + "/refresh", {
+    fetch("http://" + session.IP + "/refresh_xlms", {
         method: 'GET'
     })
-        .then(response => response.json())
+        .then(response => response.ok ? response.json() : [["NOMSGS"]],
+              error => console.log("Fetch failed: " + error))
         //.then(json => {console.log(json); return json;})
-        .then(messages => messages[0] != "NOMSGS" ? callback(messages[0]) : null);
+        .then(messages => callback(messages[0]),
+              error => console.log("JSON failed: " + error));
 }
 
 
 function _scenario_running (messages) {
     messages.forEach(msg => {
-        if (msg === "NOMSG") {return;}
+        if (msg === "NOMSGS") {return;}
         session.log.push([Date.now(), msg]);
-        console.log(msg);
-        if (msg === "ERROR=NO_IV") {
+        var split = msg.split('='),
+            prefix = split[0],
+            value = split[1];
+        if (msg === "ADMIN=FORCE_EXIT") {
+            on_sim_end();
+        } else if (msg === "ERROR=NO_IV") {
             logger("IV not in place.");
-        } else if (msg === "ADMIN=FORCE_EXIT") {
-            setTimeout(on_sim_end, 0);
-        } else {
-            var split = msg.split('='),
-                prefix = split[0],
-                value = split[1];
-            if (typeof physiology_data[prefix] !== "undefined") {
+        } else if (prefix !== "SIM_TIME") {
+            if (physiology_data.keys.includes(prefix)) {
                 physiology_data[prefix] = value;
             } else {
                 logger(msg);
@@ -83,7 +86,9 @@ function _scenario_running (messages) {
 
 
 function _in_startup (messages) {
-    for (var msg in messages) {
+    messages.forEach(msg => {
+        if (msg === "NOMSGS") {return;}
+        console.log(msg);
         var split = msg.split('='),
             prefix = split[0],
             value = split[1];
@@ -93,30 +98,36 @@ function _in_startup (messages) {
                 break;
             case "STATUS":
                 if (value === "READY") {
-                    if (session.attached_modules === session.required_modules) {
-                        logger("Sysem Ready");
+                    if (session.attached_modules.length === 0) {
+                        queue_AMM_message('ADMIN=REQUEST_MODULES');
+                        queue_AMM_message('ADMIN=REQUEST_STATUS', 1000);
+                    } else if (session.attached_modules === session.required_modules ||
+                        // If every required module is attached
+                        session.required_modules.every(module => session.attached_modules.includes(module)))
+                    {
+                        logger("System Ready");
                         ui.start_sim.disabled = false;
-                        poller_callback = _scenario_running;
-                    } else if (session.attached_modules.length === 0) {
-                        send_AMM_message('ADMIN=REQUEST_MODULES');
+                        POLLER_CALLBACK = _scenario_running;
                     } else {
                         var missing = [];
                         session.required_modules.forEach(module => {
-                            if (!session.attached_modules.contains(module)) {
+                            if (!session.attached_modules.includes(module)) {
                                 missing.push(module);
                             }
                         });
                         missing.forEach(module => {
                             logger("Missing " + module + " module.");
-                            send_AMM_message("ERROR=MISSING:" + module);
+                            queue_AMM_message("ERROR=MISSING:" + module);
                         });
-                        setTimeout(() => send_AMM_message('ADMIN=REQUEST_MODULES'), 500);
+                        queue_AMM_message('ADMIN=REQUEST_MODULES', 500);
+                        queue_AMM_message('ADMIN=REQUEST_STATUS', 1000);
                     }
                 }
                 break;
-            case "ADMIN":
+            case "SYS":
+                logger(msg);
                 switch (value) {
-                    case "START_SIM":
+                    case "START":
                         on_sim_start();
                         break;
                     case "FORCE_EXIT":
@@ -126,42 +137,48 @@ function _in_startup (messages) {
                 break;
             default:
                 session.log.push([Date.now(), msg]);
-                console.log(msg);
+                logger(msg);
         }
-    }
+    })
 }
-poller_callback = _in_startup;
+POLLER_CALLBACK = _in_startup;
 
 
-function message_poller () {
+function message_poller (delay) {
 
-    poller_ID = setTimeout(message_poller, 200);
+    delay = delay || 500;
 
-    get_AMM_messages(poller_callback);
+    POLLER_ID = setTimeout(message_poller, delay);
+
+    get_AMM_messages(POLLER_CALLBACK);
 
 }
 
 
 function kill_poller () {
-    clearTimeout(poller_ID);
+    clearTimeout(POLLER_ID);
 }
 
 
-function send_AMM_message (message) {
-    console.log("Sending: " + message);
+function queue_AMM_message (message, delay) {
 
-    // FIXME: Actual IP data location.
-    fetch('http://' + session.IP + "/action", {
-        method: 'POST',
-        headers: {
-            "Content-type": "application/json; charset=UTF-8"
-        },
-        body: JSON.stringify({action: message})
-    })
-        .then(response => response.json())
-        //.then(json => console.log(json))
+    delay = delay || 0;
+
+    console.log("Queuing: " + message);
+
+    setTimeout(() => {
+        // FIXME: Actual IP data location.
+        fetch('http://' + session.IP + "/action_xlms", {
+            method: 'POST',
+            headers: {
+                "Content-type": "application/json; charset=UTF-8"
+            },
+            body: JSON.stringify({action: message})
+        })
+            .then(response => response.json())
+            //.then(json => console.log(json))
         ;
-
+    }, delay);
 }
 
 
@@ -169,7 +186,7 @@ function on_sim_start () {
     logger("Starting Scenario.");
     ui.end_sim.disabled = false;
     ui.start_sim.disabled = true;
-    poller_callback = _scenario_running;
+    POLLER_CALLBACK = _scenario_running;
 }
 
 
@@ -177,27 +194,35 @@ function on_sim_end () {
     logger("Ending Scenario.");
     ui.end_sim.disabled = true;
     ui.start_sim.disabled = true;
-    setTimeout(kill_poller, 5000);
+    setTimeout(kill_poller, 1000);
 }
 
 
 function scenario_init (scenario) {
 
-    logger("Registering XLMS Module.");
-    send_AMM_message('KEEP_HISTORY=FALSE');
-    send_AMM_message('MODULE_NAME=XLMS');
-    send_AMM_message('REGISTER=ADMIN');
-    send_AMM_message('REGISTER=ACT');
-    send_AMM_message('REGISTER=ERROR');
-    send_AMM_message('REGISTER=MODULES');
-    send_AMM_message('REGISTER=SIM_TIME');
-    send_AMM_message('REGISTER=STATUS');
-    PHYSIOLOGY_VARIABLES.forEach(variable => send_AMM_message('REGISTER=' + variable));
-    send_AMM_message('ADMIN=LOAD_SCENARIO:' + scenario);
-    send_AMM_message('ADMIN=REQUEST_STATUS');
-    send_AMM_message('ADMIN=REQUEST_MODULES');
+    var delay = 0;
 
-    //message_poller();
+    function dont_spam (message) {
+        queue_AMM_message(message, delay);
+        delay += 100;
+    }
+
+    logger("Registering XLMS Module.");
+    dont_spam('KEEP_HISTORY=FALSE');
+    dont_spam('MODULE_NAME=XLMS');
+    dont_spam('REGISTER=ADMIN');
+    dont_spam('REGISTER=SYS');
+    dont_spam('REGISTER=ACT');
+    dont_spam('REGISTER=ERROR');
+    dont_spam('REGISTER=MODULES');
+    dont_spam('REGISTER=SIM_TIME');
+    dont_spam('REGISTER=STATUS');
+    PHYSIOLOGY_VARIABLES.forEach(variable => dont_spam('REGISTER=' + variable));
+    dont_spam('ADMIN=LOAD_SCENARIO:' + scenario);
+    dont_spam('ADMIN=REQUEST_STATUS');
+    dont_spam('ADMIN=REQUEST_MODULES');
+
+    POLLER_ID = setTimeout(() => message_poller(250), 5000);
 
 }
 
@@ -222,12 +247,12 @@ function ui_init () {
     }
 
     ui.start_sim.addEventListener('click', () => {
-        send_AMM_message("ADMIN=START_SIM");
+        queue_AMM_message("ADMIN=START_SIM");
         on_sim_start();
     });
 
     ui.end_sim.addEventListener('click', () => {
-        send_AMM_message("ADMIN=FORCE_EXIT");
+        queue_AMM_message("ADMIN=FORCE_EXIT");
         on_sim_end();
     });
 
@@ -254,18 +279,18 @@ function init () {
 
             case "session":
                 console.log(message.data.value);
-                session = message.data.value;
+                session.XLMS_data = message.data.value;
                 scenario_init(session.scenario);    // FIXME: Actual scenario data location.
                 break;
 
             case "start_exercise":
-                // TODO: Send Start message.
-
-                setTimeout(message_poller, 0);
+                queue_AMM_message("ADMIN=START_SIM");
+                on_sim_start();
                 break;
 
             case "end_exercise":
-                // TODO: Send End message.
+                queue_AMM_message("ADMIN=FORCE_EXIT");
+                on_sim_end();
                 break;
 
             case "results_request":
